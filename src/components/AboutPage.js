fd7970bd173f221e1b91faacee0a5f9783f93cb8
Jamar Gillam <jamargillam@gmail.com>
2016-12-17 16:41:43 -0600

    State is persisted in localStorage Dummy login form created Configure the relative paths in the the webpack config file

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {Link} from 'react-router';
 import '../styles/about-page.css';
 import {increment} from '../actions/login';
-console.log(increment());
 
 // Since this component is simple and static, there's no parent container for it.
 const AboutPage = () => {
--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,35 @@
 import React, { PropTypes } from 'react';
-import { Link, IndexLink } from 'react-router';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {indigo300, indigo500, grey50, grey200, grey300, grey600, amber500 } from 'material-ui/styles/colors';
+import {fade} from 'material-ui/utils/colorManipulator';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
-// This is a class-based component because the current
-// version of hot reloading won't hot reload a stateless
-// component at the top-level.
 class App extends React.Component {
+
   render() {
+    const muiTheme = getMuiTheme({
+      palette: {
+        primary1Color: indigo500,
+        primary2Color: amber500,
+        primary3Color: indigo300,
+        accent1Color: amber500,
+        accent2Color: grey200,
+        accent3Color: grey600,
+        textColor: indigo500,
+        alternateTextColor: grey50,
+        canvasColor: grey300,
+        borderColor: grey300,
+        disabledColor: fade(grey600, 0.3),
+        pickerHeaderColor: indigo500,
+        clockCircleColor: fade(grey600, 0.07),
+        shadowColor: grey300,
+      }
+  });
     return (
-      <MuiThemeProvider>
+      <MuiThemeProvider muiTheme={muiTheme}>
       <div>
-        <IndexLink to="/">Home</IndexLink>
-        {' | '}
-        <Link to="/fuel-savings">Demo App</Link>
-        {' | '}
-        <Link to="/about">About</Link>
-        <br/>
         {this.props.children}
       </div>
     </MuiThemeProvider>
